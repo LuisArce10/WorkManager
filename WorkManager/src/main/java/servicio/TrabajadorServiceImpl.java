bd320dd4b55a35af43c09b752038433cc1d24b34
LuisArce10 <luismiguelarcebenites@gmail.com>
2026-06-18 14:49:49 -0500

    borrado comentarios

--- WorkManager/src/main/java/servicio/TrabajadorServiceImpl.java
+++ WorkManager/src/main/java/servicio/TrabajadorServiceImpl.java
@@ -4,8 +4,8 @@ import entidades.Trabajador;
 import entidades.Usuario;
 import entidades.Rol;
 import repositorios.TrabajadorRepository;
-import repositorios.UsuarioRepository; // Asegúrate de que el nombre del paquete coincida
-import repositorios.RolRepository;     // Asegúrate de que el nombre del paquete coincida
+import repositorios.UsuarioRepository;
+import repositorios.RolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -56,32 +56,28 @@ public class TrabajadorServiceImpl implements TrabajadorService {
     @Override
     @Transactional
     public void save(Trabajador trabajador) {
-        // 1. Guardamos al trabajador en su tabla
+
         trabajadorRepository.save(trabajador);
 
-        // 2. Extraemos el username del correo (ej: luisa@gmail.com -> luisa)
         String usernameLimpio = trabajador.getEmail().split("@")[0].toLowerCase();
 
-        // 3. Si no existe un usuario con ese username, lo creamos
         if (!usuarioRepository.existsByUsername(usernameLimpio)) {
             Usuario nuevoUsuario = new Usuario();
             nuevoUsuario.setUsername(usernameLimpio);
             nuevoUsuario.setEnabled(true);
 
-            // Contraseña: "User" + teléfono
+
             String claveTemporal = "User" + trabajador.getTelefono();
             nuevoUsuario.setPassword(passwordEncoder.encode(claveTemporal));
 
-            // 4. Buscamos el rol ROLE_USER
+
             Rol rolUser = rolRepository.findByNombre("ROLE_USER")
                     .orElseThrow(() -> new RuntimeException("Error: El rol ROLE_USER no existe en la BD."));
 
-            // 5. Asignamos el rol
             Set<Rol> roles = new HashSet<>();
             roles.add(rolUser);
             nuevoUsuario.setRoles(roles);
 
-            // 6. Guardamos el usuario con sus roles asociados
             usuarioRepository.save(nuevoUsuario);
 
             System.out.println("=================================================");
